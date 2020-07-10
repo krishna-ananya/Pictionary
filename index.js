437a84c37ce0abda7cbd04759a1058e2c6d4cb0f
jan9209 <67917758+jan9209@users.noreply.github.com>
2020-07-10 20:45:48 +0530

    updated path

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
     // res.send('<h1>Hi, Hello world</h1>');
-    res.sendFile(__dirname + '/html5');
+    res.sendFile(__dirname + '/html5/');
 });
 
 function onConnection(socket){
-    socket.on('drawing', (data) => socket.broadcast.emit('draw',data));
+    socket.on('drawing', (data) => socket.broadcast.emit('drawing',data));
 }
 
 io.on('connection',onConnection);
