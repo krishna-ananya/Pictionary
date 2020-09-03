d8b63bbc820ac259f0b24e9e3aa597394891197e
Jayalakshmi <krishna.ananya@gmail.com>
2020-09-03 14:43:44 +0530

    scored changes

--- chat/public/script.js
+++ chat/public/script.js
@@ -1,5 +1,5 @@
-const socket = io('http://18.222.217.228:3000')
-//const socket = io('https://localhost:3000')
+//const socket = io('http://18.222.217.228:3000')
+const socket = io('https://localhost:3000')
 const messageHolder = document.getElementById('message-holder')
 const roomController = document.getElementById('room-controller')
 const sendMessageForm = document.getElementById('send-controller')
@@ -278,6 +278,9 @@ socket.on('user-disconnected', name => {
 })
 
 socket.on('updated-score', data => {
+  for(item in data){
+    console.log(item)
+;  }
   document.getElementById("scorecard").innerHTML = " Your Score is : "+data[socket.id].score
 })
 
